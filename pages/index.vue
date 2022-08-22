7c3e2b545d6fbd77c7911485260d90ceed90b768
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-08-22 19:27:54 -0400

    note dashboard card

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,63 @@
+<script>
+export default {
+    head() {
+        return {
+            bodyAttrs: {
+                class: "reset-body",
+            },
+        };
+    },
+};
+</script>
+
 <template>
-    <div>
+    <div class="dashboardContainer">
         <div class="dashboard">
-            <div class="navigationPanel">
-                <div class="notebook">Notebook</div>
-                <div class="recentNotes">Recent Notes</div>
-                <div class="noteSearch">Note Search</div>
-                <div class="noteSettings">Settings</div>
-            </div>
+            <div class="navigationPanel"></div>
+            <div class="Editor-Panel"></div>
         </div>
     </div>
 </template>
+
+<style lang="scss">
+.dashboardContainer {
+    background: #292a3b;
+    width: 100vw;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 0.5fr 1.9fr 0.6fr;
+    grid-template-rows: 0.1fr 2.8fr 0.1fr;
+    grid-auto-columns: 1fr;
+    grid-auto-rows: 1fr;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas:
+        ". . ."
+        ". dashboard ."
+        ". . .";
+}
+
+.dashboard {
+    background: #9ba8d1;
+    display: grid;
+    grid-template-columns: 0.4fr 1.6fr;
+    grid-template-rows: 1fr;
+    gap: 0px 0px;
+    grid-auto-flow: row;
+    grid-template-areas: "navigationPanel EditorPanel";
+    grid-area: dashboard;
+}
+
+.navigationPanel {
+    grid-area: navigationPanel;
+}
+
+.EditorPanel {
+    grid-area: EditorPanel;
+}
+
+.reset-body {
+    margin: 0 !important;
+    padding: 0 !important;
+}
+</style>
